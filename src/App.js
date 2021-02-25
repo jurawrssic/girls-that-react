2efee72a9a281ee7663a3250c75f27e0634e3985
Stheffany Hadlich <stheffanyhadlich@gmail.com>
2021-02-25 10:24:24 -0300

    feat: add class component

--- src/App.js
+++ src/App.js
@@ -1,53 +1,50 @@
-import { useState } from 'react'
-import Header from './components/Header'
-import AddToDo from './components/ToDoList'
-import ToDoList from './components/ToDoList'
-import './App.css';
-import AddTodo from './components/AddToDo';
+import { useState } from "react";
+import Header from "./components/Header";
+import ToDoList from "./components/ToDoList";
+import "./App.css";
+import AddTodo from "./components/AddToDo";
 
 function App() {
   const [tasks, setTasks] = useState([
     {
       id: 1,
-      text: 'Learn React',
-      done: false
+      text: "Learn React",
+      done: false,
     },
     {
       id: 2,
-      text: 'Brush the cat (meow)',
-      done: true
+      text: "Brush the cat (meow)",
+      done: true,
     },
     {
       id: 3,
-      text: 'Feed the dog (woof)',
-      done: true
-    }
-  ])
+      text: "Feed the dog (woof)",
+      done: true,
+    },
+  ]);
 
   const addTask = (text) => {
-    const id = Math.floor(Math.random() * 100000) + 1
-    const newTask = { id, text, done: false }
+    const id = Math.floor(Math.random() * 100000) + 1;
+    const newTask = { id, text, done: false };
 
-    setTasks([...tasks, newTask])
-  }
+    setTasks([...tasks, newTask]);
+  };
 
   const markAsDone = (taskID) => {
     setTasks(
-      tasks.map(task =>
-        task.id === taskID
-          ? { ...task, done: !task.done}
-          : task
+      tasks.map((task) =>
+        task.id === taskID ? { ...task, done: !task.done } : task
       )
-    )
-  }
+    );
+  };
 
   return (
     <div className="App">
       <Header />
       <div className="container">
-        <AddTodo onAdd={ addTask } />
+        <AddTodo onAdd={addTask} />
 
-        <ToDoList tasks={ tasks } onMark={ markAsDone }/>
+        <ToDoList tasks={tasks} onMark={markAsDone} />
       </div>
     </div>
   );
---
+++ src/AppWitchClass.js
@@ -0,0 +1,59 @@
+import React from "react";
+import Header from "./components/Header";
+import ToDoList from "./components/ToDoList";
+import "./App.css";
+import AddTodo from "./components/AddToDo";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = [
+      {
+        id: 1,
+        text: "Learn React",
+        done: false,
+      },
+      {
+        id: 2,
+        text: "Brush the cat (meow)",
+        done: true,
+      },
+      {
+        id: 3,
+        text: "Feed the dog (woof)",
+        done: true,
+      },
+    ];
+  }
+
+  addTask = (text) => {
+    const id = Math.floor(Math.random() * 100000) + 1;
+    const newTask = { id, text, done: false };
+
+    this.setState([...this.state, newTask]);
+  };
+
+  markAsDone = (taskID) => {
+    this.setState(
+      this.state.map((task) =>
+        task.id === taskID ? { ...task, done: !task.done } : task
+      )
+    );
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <div className="container">
+          <AddTodo onAdd={this.addTask} />
+
+          <ToDoList tasks={this.state} onMark={this.markAsDone} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
